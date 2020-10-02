5444c7f91ebdd6ae176d152160d594c87f807f3e
Dmitrii Efremov <dmefr.mail@gmail.com>
2020-10-02 18:06:26 +0300

    Updated acc_class.py
    commp_accs, chng e-mail functions and accnt property added

--- acc_class.py
+++ acc_class.py
@@ -1,9 +1,16 @@
+import re
+
 class Acc(object):
     def __init__(self,page,email,psswd):
         self.page=page
         self.email=email
         self.psswd=psswd
 
+    def __repr__(self):
+        return("Account for {} at {}".format(self.email,self.page))
+    def __str__(self):
+        return("{}, {}, {}".format(self.page,self.email,self.psswd))
+    
     def get_page(self):
         return(self.page)
     def get_email(self):
@@ -11,18 +18,26 @@ class Acc(object):
     def get_psswd(self):
         return(self.psswd)
     
-    def __str__(self):
-        return("{}, {}, {}".format(self.page,self.email,self.psswd))
-
+    
     def chng_psswd(self,psswd):
-        if type(psswd)==str:
-            self.psswd=psswd
+        pswwd=str(psswd)
+        if len(pswwd)==0:
+            print("No password provided")
+        elif len(pswwd)<=6:
+            print("Password too short")
         else:
-            pass
+            self.psswd=pswwd
 
 tst=Acc("github.com","someone@mail.tr","123")
 print(tst.get_page(), tst.get_email())
 print(str(tst))
+print(repr(tst))
 
-tst.chng_psswd("newpass11")
+dir(tst)
+
+tst.chng_psswd("")
+tst.chng_psswd(1234)
+newpass="newpass123"
+tst.chng_psswd(newpass)
+
 print(tst.get_psswd())
