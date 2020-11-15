adca4b2cde10b112decce1ae681f51d4f65dff5a
Dmitrii Efremov <dmefr.mail@gmail.com>
2020-11-15 17:30:41 +0300

    Some changes to Acc_class
    Added some functionality to acc class to for supporting more than 1 email

--- acc_class.py
+++ acc_class.py
@@ -1,10 +1,17 @@
 import re
 
+def check_email(email):
+    '''function checks if passed email corresponds to overall email syntax'''
+    pass
+
 class Acc(object):
     def __init__(self,page,email,psswd):
+        assert type(page)==str, 'Incorrect email type, should be string'
+        assert type(email)==str, 'Incorrect email type should be string'
         self.page=page
-        self.email=email
+        self.email=[]
         self.psswd=psswd
+        self.email.append(email)
 
     @property
     def psswd(self):
@@ -12,24 +19,34 @@ class Acc(object):
     
     @psswd.setter
     def psswd(self, psswd):
-        assert len(psswd)>5, "password must be at least 6 characters long"
+        assert len(psswd)>=8, "password must be at least 6 characters long"
         self.__psswd=psswd
 
     def __repr__(self):
-        return("Account for {} at {}".format(self.email,self.page))
+        return("Account for {} at {}".format(self.email[0],self.page))
     def __str__(self):
-        return("{}, {}, {}".format(self.page,self.email,self.psswd))
+        return("{}, {}, {}".format(self.page,self.email[0],self.psswd))
     
+    #some small class methods
     def get_page(self):
         return(self.page)
+        
     def get_email(self):
-        return(self.email)
+        assert len(self.email)>=1, 'No email provided'
+        if len(self.email)==1:
+            return(self.email[0])
+        else:
+            if len(self.email)==2:
+                return("{}, {}".format(self.email[0],self.email[1]))
+            elif len(self.email)==3:
+                return("{}, {}, {}".format(self.email[0],self.email[1],self.email[2]))
+    
     def get_psswd(self):
         return(self.psswd)
 
     @property
     def accnt(self):
-        return(self.email,self.page)
+        return(self.email[0],self.page)
     
     def comp_accs(self,other):
         if not isinstance (other, Acc):
@@ -46,19 +63,27 @@ class Acc(object):
         else:
             self.psswd=pswwd
 
-    def chng_email(self,email):
-        if type(email)==str:
-            #need to add re-check here!!!
-            self.email=email
+    def chng_email(self,email,primary=True):
+        assert type(email)==str, 'Wrong email type'
+        if primary==True:
+            #need to add re check here!!!
+            self.email[0]=email
         else:
-            pass
+            if len(self.email)>3:
+                print('This account already has three email adresses, canot add more')
+            else:
+                self.email.append(email)
+            
 
-tst=Acc("github.com","someone@mail.tr","123456")
+
+
+
+tst=Acc("github.com","someone@mail.tr","12345678")
 print(tst.get_page(), tst.get_email())
 print(tst.accnt)
 print(str(tst))
 print(repr(tst))
-tst2=Acc("github.com","someone@mail.tr","123456")
+tst2=Acc("github.com","someone@mail.tr","12345678")
 
 print(tst.comp_accs(tst2))
 tst.chng_psswd("")
@@ -67,3 +92,10 @@ newpass="newpass123"
 tst.chng_psswd(newpass)
 
 print(tst.get_psswd())
+
+tst.chng_email('some1else@mail.com')
+print(tst.get_email())
+tst.chng_email('anotheremail@mail.hz',primary=False)
+print(tst.get_email())
+tst.chng_email('andonemore@mail.hh',primary=False)
+print(tst.get_email())
